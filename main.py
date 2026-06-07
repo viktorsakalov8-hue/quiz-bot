c93d9c8237d09b8250c2519aab26a8175f410a82
soratnikov <soratnikov@gmail.com>
2026-06-07 15:25:52 +0500

    update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import asyncio
-try:
-    asyncio.get_event_loop()
-except RuntimeError:
-    asyncio.set_event_loop(asyncio.new_event_loop())
-
-import pyrogram
 from pyrogram import Client, filters, enums
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 import json
